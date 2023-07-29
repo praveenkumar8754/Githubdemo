b81e6ee938aba973da897758fb6f07a80afc4908
praveenkumar8754 <praveenraman7877@gmail.com>
2023-07-29 13:22:15 +0530

    code updated

---
+++ array valueOf()methods.js
@@ -0,0 +1,5 @@
+//The valueOf() method returns the array itself.
+//The valueOf() method does not change the original array.
+//fruits.valueOf() returns the same as fruits.
+var a = ["apple","orange","banan","mango"];
+console.log(a.valueOf());
